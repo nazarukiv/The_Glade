e466f4eff6da8ff470a97811f02cde04d4ed4970
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-04-24 22:04:50 +0100

    debug-toolbar added

--- app/settings.py
+++ app/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "debug_toolbar",
     'main',
-    'goods'
+    'goods',
 
 ]
 
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -122,6 +125,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+]
+
+INTERNAL_IPS =[
+    "127.0.0.1"
 ]
 
 
--- app/urls.py
+++ app/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('catalog/', include('goods.urls', namespace='catalog'))
+    path('catalog/', include('goods.urls', namespace='catalog')),
+    path("__debug__/", include('debug_toolbar.urls')),
 ]
+
+
+
 
 """
 www.example.com/admin

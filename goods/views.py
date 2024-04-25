9cf9118172628055e15710943de9b1f9d7e91416
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-04-25 18:26:53 +0100

    Pagination added

--- goods/views.py
+++ goods/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator
 from goods.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == "all-goods":
         goods = Products.objects.all()
     else:
         goods = Products.objects.filter(category__slug=category_slug)
 
-
+    paginator = Paginator(goods, per_page=3)
+    current_page = paginator.page(page)
 
 
     context = {
         'title': 'Catalog',
-        'goods': goods}
+        'goods': current_page,
+        'slug_url': category_slug
+    }
 
     return render(request, 'goods/catalog.html', context)
 

387fb730c957b4d5f5704bdc8c281f0138ead88f
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-04-25 15:28:00 +0100

    Register simple tag for categories

--- main/views.py
+++ main/views.py
@@ -4,12 +4,10 @@ from goods.models import Categories
 
 
 def index(request):
-    categories = Categories.objects.all()
 
     context = {
         'title': 'Home - Main Page',
         'content': 'Urban Era Emporium',
-        'categories': categories
     }
 
     return render(request, 'main/index.html', context)

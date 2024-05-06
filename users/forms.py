b854ed5c229cbe23cb9c878a49da826ccee88fb9
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-05-06 15:34:52 +0100

    Login form and controller done

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
-from users.models import User
-
-
-
-
-class UserLoginForm(forms.ModelForm):
+class UserLoginForm(AuthenticationForm):
     username = forms.CharField(
         label='Username',
         widget=forms.TextInput(attrs={'autofocus': True,
@@ -21,10 +16,6 @@ class UserLoginForm(forms.ModelForm):
                                           })
     )
 
-
-    class Meta:
-        model = User
-        fields = ['username', 'password']
 
 
 
--- users/views.py
+++ users/views.py
@@ -1,22 +1,21 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
-from users.forms import UserLoginForm
 from django.contrib import auth
-
+from django.contrib.auth import login as auth_login
+from users.forms import UserLoginForm
 # Create your views here.
 
 def login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
+            user = form.get_user()
             if user:
-                auth.login(request, user)
+                auth_login(request, user)
                 return HttpResponseRedirect(reverse('main:index'))
+            else:
+                form.add_error(None, "Invalid username or password")
     else:
         form = UserLoginForm()
 

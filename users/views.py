8aa4d162f19e6b018485ac3f98e88989bc979ade
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-05-06 14:04:11 +0100

    Simple log in form added

--- users/views.py
+++ users/views.py
@@ -1,10 +1,28 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+from django.contrib import auth
 
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Authorization'
+        'title': 'Authorization',
+        'form': form
     }
 
     return render(request, 'users/login.html', context)
